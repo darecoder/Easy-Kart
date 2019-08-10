4aafcd77d4e621017087b2fbe15df0689c314d34
ekta1999 <ektamishra1999@gmail.com>
2019-08-10 22:17:19 +0530

    dao,service,entities added

---
+++ src/main/java/com/example/EasyKart/dao/AccountDAO.java
@@ -0,0 +1,21 @@
+package com.example.EasyKart.dao;
+
+import com.example.EasyKart.entity.Account;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+
+@Transactional
+@Repository
+public class AccountDAO {
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    public Account findAccount(String userName) {
+        Session session = this.sessionFactory.getCurrentSession();
+        return session.find(Account.class, userName);
+    }
+}
